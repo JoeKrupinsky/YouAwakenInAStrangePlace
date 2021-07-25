30894c62b9e615d474b81d6878fe1f1430e564f1
Joe Krupinsky <68128086+JoeKrupinsky@users.noreply.github.com>
2021-07-25 15:57:15 -0500

    World Card work

--- src/components/world/WorldCard.jsx
+++ src/components/world/WorldCard.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Button, Col, Row, Card } from "react-bootstrap";
+import React, {useState, useEffect} from "react";
+import {Button, Col, Row, Card, FormControl, Container} from "react-bootstrap";
+import {Formik} from "formik";
 export const WorldCard = (props) => {
   const [statements, setStatements] = useState(props.world.statements);
   const [formattedStatements, setFormattedStatements] = useState([]);
@@ -9,7 +10,7 @@ export const WorldCard = (props) => {
     statements.forEach((statement) =>
       newArr.push(
         <Row key={counter++}>
-          <h5>-{statement.toUpperCase()}</h5>
+          <h4>-{statement.toUpperCase()}</h4>
         </Row>
       )
     );
@@ -19,13 +20,16 @@ export const WorldCard = (props) => {
   if (formattedStatements.length == 0) {
     formatStatements();
   }
-  const addFact = (e) => {
-    e.preventDefault();
-    console.log(e);
+  const addFact = (fact) => {
+    let newStatements = statements;
+    newStatements.push(fact.name);
+    setStatements(newStatements);
+    formatStatements();
   };
+
   return (
     <React.Fragment>
-      <Card style={{ fontFamily: "TypeWriter" }}>
+      <Card style={{fontFamily: "TypeWriter"}}>
         <Card.Header>
           <h1>
             The {props.world.adjective} {props.world.location}: A{" "}
@@ -33,13 +37,53 @@ export const WorldCard = (props) => {
           </h1>
         </Card.Header>
         <Card.Body>
-          <Row>
-            <h2>Facts About This World</h2>
-          </Row>
-          {formattedStatements}
-          <Row>
-           
-          </Row>
+          <Container>
+            <Row>
+              <h2>Facts About This World</h2>
+            </Row>
+            {formattedStatements}
+            </Container>
+            <Row style={{borderTop: "2px solid black",paddingTop:'10px'}}>
+              <Col>ADD NEW FACTS</Col>
+              <Col>
+                <Formik
+                  initialValues={{name: ""}}
+                  onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                      let valCopy = values;
+                      addFact(valCopy);
+                      actions.resetForm();
+                    }, 1000);
+                  }}
+                >
+                  {(props) => (
+                    <form onSubmit={props.handleSubmit}>
+                      <Row>
+                        <Col>
+                          {" "}
+                          <FormControl
+                            type="text"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.name}
+                            name="name"
+                          />
+                          {props.errors.name && (
+                            <div id="feedback">{props.errors.name}</div>
+                          )}
+                        </Col>
+                        <Col>
+                          <Button variant="primary" type="submit">
+                            Submit
+                          </Button>
+                        </Col>
+                      </Row>
+                    </form>
+                  )}
+                </Formik>
+              </Col>
+            </Row>
+          
         </Card.Body>
       </Card>
     </React.Fragment>
